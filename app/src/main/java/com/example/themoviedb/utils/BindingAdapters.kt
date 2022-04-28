394ec809a8b60ac6ebac3687ec3e9e1e7f88f3b1
noorbhtiti <43866697+noorbhtiti@users.noreply.github.com>
2022-04-28 18:41:17 +0200

    Update BindingAdapters.kt

--- app/src/main/java/com/example/themoviedb/utils/BindingAdapters.kt
+++ app/src/main/java/com/example/themoviedb/utils/BindingAdapters.kt
@@ -1,14 +1,8 @@
 package com.example.themoviedb.utils
 
-import android.content.Intent
-import android.net.Uri
-import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
-import androidx.core.content.ContextCompat.startActivity
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
-import com.example.themoviedb.model.Genres
 
 
 @BindingAdapter("posterImageUrl")
@@ -17,7 +11,7 @@ fun bindPosterImage(imgView: ImageView,imgUrl:String){
         Glide
             .with(imgView)
             .load(Constants.POSTER_URL + Constants.POSTER_SIZE + posterPath)
-            .into(imgView);
+            .into(imgView)
     }
 }
 
@@ -27,6 +21,6 @@ fun bindBackdropImage(imgView: ImageView, imgUrl:String) {
         Glide
             .with(imgView)
             .load(Constants.BACKDROP_IMAGE_BASE_URL + Constants.BACKDROP_IMAGE_WIDTH + backdropPath)
-            .into(imgView);
+            .into(imgView)
     }
 }
